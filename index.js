5fe8ca75c960169c6826494ac8f3aed93988db93
Rémi Lebret <remi.lebret@ext.adeoservices.com>
2015-06-03 23:55:11 +0200

    test oracledb

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('first success');
+console.log('start');
+
+var oracledb = require('oracledb');
+
+oracledb.getConnection(
+  {
+    user          : "hr",
+    password      : "welcome",
+    connectString : "localhost/XE"
+  },
+  function(err, connection)
+  {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    connection.execute(
+      "SELECT 1, :did as id "
+    + "FROM dual ",
+      [180],
+      function(err, result)
+      {
+        if (err) {
+          console.error(err.message);
+          return;
+        }
+        console.log(result.rows);
+      });
+  });
